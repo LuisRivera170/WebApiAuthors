89eb9122858e0e6d81ef05da60956d578f91809e
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2022-08-25 17:46:18 -0500

    Model validations

--- WebApiAutores/Entities/Author.cs
+++ WebApiAutores/Entities/Author.cs
@@ -1,9 +1,17 @@
-﻿namespace WebApiAutores.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiAutores.Entities
 {
     public class Author
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(maximumLength: 15, ErrorMessage = "The length field {0} cannot be more than {1}")]
         public string Name { get; set; }
+        [Range(18, 100)]
+        [NotMapped]
+        public int Age { get; set; }
         public List<Book> Books { get; set; }
     }
 }
