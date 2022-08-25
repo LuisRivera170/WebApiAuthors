3e12d7da1d512d484e7c5daedf2a2bb72a13504c
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2022-08-25 12:34:02 -0500

    Authors controller

---
+++ WebApiAutores/Controllers/AuthorsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entities;
+
+namespace WebApiAutores.Controllers
+{
+
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Author>> GetAuthors() {
+            return new List<Author>() {
+                new Author()
+                {
+                    Id = 1,
+                    Name = "Felipe"
+                },
+                new Author()
+                {
+                    Id = 2,
+                    Name = "Claudia"
+                }
+            };
+        }
+    }
+}
+
