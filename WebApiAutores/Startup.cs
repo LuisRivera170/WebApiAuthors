343f3e103cbe695ddb091a574684769f1c46ba52
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2022-08-29 13:53:27 -0500

    Swagger configuration for sending JWT

--- WebApiAutores/Startup.cs
+++ WebApiAutores/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using System.Text.Json.Serialization;
 using WebApiAutores.Filters;
@@ -49,7 +50,32 @@ namespace WebApiAutores
                 );
 
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new String[] {}
+                    }
+                });
+            });
 
             services.AddAutoMapper(typeof(Startup));
 
