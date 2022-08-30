82354352646e8f745f059711211892be96c62c69
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2022-08-30 16:35:41 -0500

    Complementing unit test cases

--- WebApiAutores.Tests/UnitTests/FirstCapitalLetterAttributeTest.cs
+++ WebApiAutores.Tests/UnitTests/FirstCapitalLetterAttributeTest.cs
@@ -20,5 +20,35 @@ namespace WebApiAutores.Tests.UnitTests
             // Verification
             Assert.AreEqual("First letter must be uppercase", result.ErrorMessage);
         }
+
+        [TestMethod]
+        public void NullValueDoesNotReturnError()
+        {
+            // Preparation
+            var firstLetterAttribute = new FirstCapitalLetterAttribute();
+            string value = null;
+            var valContext = new ValidationContext(new { Name = value });
+
+            // Execution
+            var result = firstLetterAttribute.GetValidationResult(value, valContext);
+
+            // Verification
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void FirstLetterInUpperCaseDoesNotReturnError()
+        {
+            // Preparation
+            var firstLetterAttribute = new FirstCapitalLetterAttribute();
+            string value = "Franz Kafka";
+            var valContext = new ValidationContext(new { Name = value });
+
+            // Execution
+            var result = firstLetterAttribute.GetValidationResult(value, valContext);
+
+            // Verification
+            Assert.IsNull(result);
+        }
     }
 }
