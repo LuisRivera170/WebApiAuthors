a1dcbf392366793be3cd0c096e8aa99021b75851
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2022-08-30 17:21:18 -0500

    Mocking dependencies with moq library

--- WebApiAutores.Tests/UnitTests/RootControllerTest.cs
+++ WebApiAutores.Tests/UnitTests/RootControllerTest.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Security.Claims;
 using WebApiAutores.Controllers.V1;
 using WebApiAutores.Tests.Mocks;
 
@@ -33,6 +36,32 @@ namespace WebApiAutores.Tests.UnitTests
             authorizationServiceMock.Result = AuthorizationResult.Failed();
             var rootController = new RootController(authorizationServiceMock);
             rootController.Url = new URLHelperMock();
+
+            // Execution
+            var result = await rootController.Get();
+
+            // Verification
+            Assert.AreEqual(4, result.Value.Count());
+        }
+
+        [TestMethod]
+        public async Task IfUserIsNotAdminGetResponsWith4LinksUsingMoqLibrary()
+        {
+            // Preparation
+            var mockAuthorizationService = new Mock<IAuthorizationService>();
+            mockAuthorizationService
+                .Setup(x => x.AuthorizeAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<object>(), It.IsAny<IEnumerable<IAuthorizationRequirement>>()))
+                .Returns(Task.FromResult(AuthorizationResult.Failed()));
+            mockAuthorizationService
+                .Setup(x => x.AuthorizeAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<object>(), It.IsAny<string>()))
+                .Returns(Task.FromResult(AuthorizationResult.Failed()));
+            var mockUrlHelper = new Mock<IUrlHelper>();
+            mockUrlHelper
+                .Setup(x => x.Link(It.IsAny<String>(), It.IsAny<object>()))
+                .Returns(String.Empty);
+
+            var rootController = new RootController(mockAuthorizationService.Object);
+            rootController.Url = mockUrlHelper.Object;
 
             // Execution
             var result = await rootController.Get();
