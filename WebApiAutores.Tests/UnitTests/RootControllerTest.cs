61f1d08b93afd831f5335c32634cc73a6ea2bf96
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2022-08-30 17:04:50 -0500

    Unit test for classes with dependecies - Mocking dependencies

---
+++ WebApiAutores.Tests/UnitTests/RootControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using WebApiAutores.Controllers.V1;
+using WebApiAutores.Tests.Mocks;
+
+namespace WebApiAutores.Tests.UnitTests
+{
+
+    [TestClass]
+    public class RootControllerTest
+    {
+
+        [TestMethod]
+        public async Task IfUserIsAdminGetResponsWith4Links()
+        {
+            // Preparation
+            var authorizationServiceMock = new AuthorizationServiceMock();
+            authorizationServiceMock.Result = AuthorizationResult.Success();
+            var rootController = new RootController(authorizationServiceMock);
+            rootController.Url = new URLHelperMock();
+
+            // Execution
+            var result = await rootController.Get();
+
+            // Verification
+            Assert.AreEqual(5, result.Value.Count());
+        }
+
+        [TestMethod]
+        public async Task IfUserIsNotAdminGetResponsWith4Links()
+        {
+            // Preparation
+            var authorizationServiceMock = new AuthorizationServiceMock();
+            authorizationServiceMock.Result = AuthorizationResult.Failed();
+            var rootController = new RootController(authorizationServiceMock);
+            rootController.Url = new URLHelperMock();
+
+            // Execution
+            var result = await rootController.Get();
+
+            // Verification
+            Assert.AreEqual(4, result.Value.Count());
+        }
+
+    }
+}
